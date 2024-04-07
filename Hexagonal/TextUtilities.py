d35c2e858cbde2313d68bcb305e2574792480c6d
Kenza <kenza.merzouk@univ-tlse3.fr>
2024-04-07 14:12:42 +0200

    ajout gestion d'erreur dans les méthodes de texte

--- Hexagonal/TextUtilities.py
+++ Hexagonal/TextUtilities.py
@@ -1,9 +1,13 @@
 class TextUtilities:
     @staticmethod
     def mirror_text(text):
+        if not isinstance(text, str):
+            raise TypeError("Le texte doit être une chaîne de caractères.")
         return text[::-1]
 
     @staticmethod
     def is_palindrome(text):
+        if not isinstance(text, str):
+            raise TypeError("Le texte doit être une chaîne de caractères.")
         cleaned_text = text.lower().replace(" ", "")
         return cleaned_text == cleaned_text[::-1]
